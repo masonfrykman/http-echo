a7f55ce0f55c5af018030ca65f556f53a0ff84cb
masonfrykman <masonfrykman@proton.me>
2025-05-22 22:06:58 -0500

    bring up server, accept connections

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,11 @@
 #include <fstream>
 #include <iostream>
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <cstring>
+
 #include "misc/config.hpp"
 
 int main(int argc, char const *argv[])
@@ -20,7 +25,33 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    
+    auto addrStr = he::config::store.at("Address");
+    auto portStr = he::config::store.at("Port");
+
+    // Bring up the server
+
+    struct addrinfo hint;
+    struct addrinfo *bindAddress;
+
+    memset(&hint, 0, sizeof(struct addrinfo));
+    hint.ai_socktype = SOCK_STREAM;
+    hint.ai_flags = AI_PASSIVE;
+
+    // TODO: Error checking on the socket operations
+    int lookup = getaddrinfo(addrStr.c_str(), portStr.c_str(), &hint, &bindAddress);
+
+    int sock = socket(bindAddress->ai_protocol, SOCK_STREAM, 0);
+
+    int bindOp = bind(sock, bindAddress->ai_addr, bindAddress->ai_addrlen);
+
+    int listenOp = listen(sock, 15);
+
+    while(true) {
+        struct sockaddr acceptedSockaddr;
+        socklen_t acceptedSocklen;
+
+        int acceptedSocket = accept(sock, &acceptedSockaddr, &acceptedSocklen);
+    }
 
     return 0;
 }
