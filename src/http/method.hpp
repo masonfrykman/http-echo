7256ac6564aeaf9509d46e1219e8d5aa5bd497bd
masonfrykman <masonfrykman@proton.me>
2025-05-25 11:48:33 -0500

    method, begin request

---
+++ src/http/method.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace he::http {
+    enum Method {
+        GET, HEAD, POST, DELETE, PUT, OPTIONS
+    };
+}
---
+++ src/http/request.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace he::http {
+    class Request {};
+}
